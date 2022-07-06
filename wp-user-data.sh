a6dbdbefe2c9cc073ed8474cee710d8d7ffac1e5
Neil Davies <neil.davies@mobilise.cloud>
2022-07-06 11:14:23 +0100

    Updated user-data script

--- wp-user-data.sh
+++ wp-user-data.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-# AUTOMATIC WORDPRESS INSTALLER IN  AWS LINUX 2 AMI
-# CHANGE DATABASE VALUES BELOW IF YOU WISH TO
+# Configure an Amazon Linux 2 instance for Wordpress
 
-#install LAMP Server
+# install all available updated
 yum update -y
-#install apache server and start it for the health checks to work
+# install apache web server and start it for the health checks to work
 yum install -y httpd
 systemctl start httpd.service
 
-# enable php7.xx from  amazon-linux-extra and install it
+# enable php7.xx from amazon-linux-extra and install it
 amazon-linux-extras enable php7.4
 yum clean metadata
 yum install -y php php-{pear,cgi,common,curl,mbstring,gd,mysqlnd,gettext,bcmath,json,xml,fpm,intl,zip,imap,devel} git unzip
 
-#install imagick extension
+# install imagick extension for wordpress
 yum -y install gcc ImageMagick ImageMagick-devel ImageMagick-perl
 pecl channel-update pecl.php.net
 printf "\n" | pecl install imagick
@@ -35,17 +34,16 @@ wget https://github.com/mobilise-academy/wordpress/archive/refs/heads/main.zip -
 cd /tmp
 unzip /tmp/wordpress.zip
 mv wordpress-main/* /var/www/html
-cd /vaw/www/html
+cd /var/www/html
 
-# Change permission of /var/www/html/
+# change permission of /var/www/html directory
 chown -R ec2-user:apache /var/www/html
-chmod -R 774 /var/www/html
 
-# enable .htaccess files in Apache config using sed command
+# enable .htaccess files in apache config using sed command
 
 sed -i '/<Directory "\/var\/www\/html">/,/<\/Directory>/ s/AllowOverride None/AllowOverride all/' /etc/httpd/conf/httpd.conf
 
-# Make apache and mysql to autostart and restart apache
+# set apache web server service to auto-start and restart to load the latest configuration
 
 systemctl enable httpd.service
 systemctl restart httpd.service
