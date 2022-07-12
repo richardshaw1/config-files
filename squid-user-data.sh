4925a5ef2ae45a7a1c5ff08440f1248e4306654a
Neil Davies <neil.davies@mobilise.cloud>
2022-07-12 17:03:28 +0100

    Squid user-data script

---
+++ squid-user-data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# DO NOT EDIT BELOW THIS LINE
+
+sudo snap start amazon-ssm-agent
+sudo apt-get update
+sudo apt-get install squid -y
+
+wget https://raw.githubusercontent.com/mobilise-academy/config-files/main/squid.conf -O /etc/squid/squid.conf
+
+sudo systemctl start squid
