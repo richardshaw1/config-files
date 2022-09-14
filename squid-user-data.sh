9022857fb67c07adf1155ce42745825c8767f3a5
richardshaw1 <103503857+richardshaw1@users.noreply.github.com>
2022-09-14 15:21:44 +0100

    Update squid-user-data.sh

--- squid-user-data.sh
+++ squid-user-data.sh
@@ -6,6 +6,6 @@ sudo snap start amazon-ssm-agent
 sudo apt-get update
 sudo apt-get install squid -y
 
-wget https://raw.githubusercontent.com/mobilise-academy/config-files/main/squid.conf -O /etc/squid/squid.conf
+wget https://raw.githubusercontent.com/richardshaw1/config-files/main/squid.conf -O /etc/squid/squid.conf
 
 sudo systemctl start squid
